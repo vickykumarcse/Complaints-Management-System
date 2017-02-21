bca353c4d0d665c46cdac9a1fc743961fdd9dae6
vickykumar10355 <vicky_kumar@persistent.co.in>
2017-02-21 18:05:49 +0530

    turbo link removed

--- Gemfile
+++ Gemfile
@@ -22,8 +22,6 @@ gem 'auth0'
 gem 'omniauth-google-oauth2', '~> 0.2.1'
 
 
-gem 'jquery-turbolinks' 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
 # Use sqlite3 as the database for Active Record
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require masonry/jquery.masonry
 //= require bootstrap-sprockets
+//= require turbolinks
 //= require_tree .
