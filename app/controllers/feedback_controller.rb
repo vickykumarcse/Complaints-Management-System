ba5447da33b493c8ca5e313b74951a351e771fd0
vickykumar10355 <vicky_kumar@persistent.co.in>
2017-02-23 11:01:00 +0530

    feedback  view  new changed

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -2,10 +2,10 @@ class FeedbackController < ApplicationController
 	def new
 		@feedback=Feedback.new(:name => params[:name], :rating => params[:rating], :comment => params[:comment])
 		if @feedback.save
-			notice="Feedback submitted successfully"
-			redirect_to "https://cms-marauders.herokuapp.com"
+			flash[:notice]="Thank you for your feedback!"
+			
 		else
-			notice="Feedback submission error!"
+			flash[:notice]="Feedback submission error!"
 		end
 	end
 
