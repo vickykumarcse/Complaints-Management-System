807b4bf838bbd6cf2437ad81265a2c1230acbf0e
vickykumar10355 <vicky_kumar@persistent.co.in>
2017-02-23 11:04:55 +0530

    feedback  view  new changed

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,12 +1,15 @@
 class FeedbackController < ApplicationController
 	def new
-		@feedback=Feedback.new(:name => params[:name], :rating => params[:rating], :comment => params[:comment])
-		if @feedback.save
-			flash[:notice]="Thank you for your feedback!"
-			
-		else
-			flash[:notice]="Feedback submission error!"
-		end
+		if params[:name] !=""
+			@feedback=Feedback.new(:name => params[:name], :rating => params[:rating], :comment => params[:comment])
+			if @feedback.save
+				flash[:notice]="Thank you for your feedback!"
+				
+			else
+				flash[:notice]="Feedback submission error!"
+			end
+	     end
+	     
 	end
 
 	def index
